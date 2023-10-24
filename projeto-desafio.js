4917ab48dee2a37539022ff80053bff1380d4ffd
daniel <danielcooder@gmail.com>
2023-10-24 19:19:08 -0300

    projeto

---
+++ projeto-desafio.js
@@ -0,0 +1,21 @@
+let nome = "Super Boy";
+let xp = 3500;
+
+
+if (xp < 1000) {
+    console.log("O Herói de nome " + nome + " está no nivel de Ferro")
+} else if (xp >= 1001 && xp <= 2000) {
+    console.log("O Herói de nome " + nome + " está no nivel de Bronze")
+} else if (xp >= 2001 && xp <= 5000) {
+    console.log("O Herói de nome " + nome + " está no nivel de Prata")
+} else if (xp >= 6001 && xp <= 7000) {
+    console.log("O Herói de nome " + nome + " está no nivel de Ouro")
+} else if (xp >= 7001 && xp <= 8000) {
+    console.log("O Herói de nome " + nome + " está no nivel de Platina")
+} else if (xp >= 8001 && xp <= 9000) {
+    console.log("O Herói de nome " + nome + " está no nivel de Ascendente")
+} else if (xp >= 9001 && xp <= 10000) {
+} else {
+    (xp >= 10001)
+    console.log("O Herói de nome " + nome + " está no nivel de Radiante")
+}
